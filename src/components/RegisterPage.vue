40c42f4b40402f573392d389309ee160fafc781e
mrredo <mrredogt@gmail.com>
2023-01-06 22:04:58 +0200

    made register page with errors

--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -6,70 +6,80 @@
     import { ReqMake } from "../functions/request"
 import { def } from "@vue/shared"
     
-    async function REGBTN(ev: MouseEvent) {
-        let user = document.getElementById("userR") as HTMLInputElement,
-            password = document.getElementById("passR") as HTMLInputElement
-        // let res1 = fetch(data.url + "auth/register", {
-        //     method: "POST",
-        //     body: JSON.stringify({
-        //         name: user.value,
-        //         password: password.value
-        //     }),
-        //     credentials: "include",
-        // }).then((res) => {
-        //     if(res.status == 422) {
-        //         console.log(10101010)
-        //     }
-        //     res.json().then((val) => {
-        //         console.log(val)
-        //     })
-        // }).catch((res) => {
-        //     res.json().then((val: any) => {
-        //         console.log(val)
-        //     })
-        // })
+//     async function REGBTN(ev: MouseEvent) {
+//         let user = document.getElementById("userR") as HTMLInputElement,
+//             password = document.getElementById("passR") as HTMLInputElement
+//         // let res1 = fetch(data.url + "auth/register", {
+//         //     method: "POST",
+//         //     body: JSON.stringify({
+//         //         name: user.value,
+//         //         password: password.value
+//         //     }),
+//         //     credentials: "include",
+//         // }).then((res) => {
+//         //     if(res.status == 422) {
+//         //         console.log(10101010)
+//         //     }
+//         //     res.json().then((val) => {
+//         //         console.log(val)
+//         //     })
+//         // }).catch((res) => {
+//         //     res.json().then((val: any) => {
+//         //         console.log(val)
+//         //     })
+//         // })
 
-        fetch(data.url + "cookie", {}).then((res) => { res.text().then((str) => console.log(str)) })
-        let res = await axios.post(data.url + "auth/register", {
-            name: user.value,
-            password: password.value
-        })
-        let errL = {
-            user: document.getElementById("userINVALID") as HTMLSpanElement,
-            pass: document.getElementById("passINVALID") as HTMLSpanElement,
-            reg: document.getElementById("regINVALID") as HTMLSpanElement,
-        }
-        let err = {
-            "1": errL.user,
-            "2": errL.pass,
-            "3": errL.reg,
-            "4": errL.reg,
-        }
-        console.log(await res.data)
-        // res.then((res: AxiosResponse) => {
-        //     if(res.status == 200) {
-        //         router.push("/")
-        //     } else {
-        //         let errRList = (res.data.error as string).split("_")
-        //         let errNum = errRList[errRList.length - 1]
+//         fetch(data.url + "cookie", {}).then((res) => { res.text().then((str) => console.log(str)) })
+//         let res = await axios.post(data.url + "auth/register", {
+//             name: user.value,
+//             password: password.value
+//         })
+//         let errL = {
+//             user: document.getElementById("userINVALID") as HTMLSpanElement,
+//             pass: document.getElementById("passINVALID") as HTMLSpanElement,
+//             reg: document.getElementById("regINVALID") as HTMLSpanElement,
+//         }
+//         let err = {
+//             "1": errL.user,
+//             "2": errL.pass,
+//             "3": errL.reg,
+//             "4": errL.reg,
+//         }
+//         console.log(await res.data)
+//         // res.then((res: AxiosResponse) => {
+//         //     if(res.status == 200) {
+//         //         router.push("/")
+//         //     } else {
+//         //         let errRList = (res.data.error as string).split("_")
+//         //         let errNum = errRList[errRList.length - 1]
 
-        //         let eleSp = (err as any)[errNum] as HTMLSpanElement
-        //         eleSp.textContent = res.data.error_message
-        // }
-        // })
-        // res.catch((res) => {
-        //     console.log(res.data)
-        // })
-}
+//         //         let eleSp = (err as any)[errNum] as HTMLSpanElement
+//         //         eleSp.textContent = res.data.error_message
+//         // }
+//         // })
+//         // res.catch((res) => {
+//         //     console.log(res.data)
+//         // })
+// }
 
 </script>
 
 <script lang="ts">
+type strDataErr = "user" | "pass" | "reg"
 export default {
     data() {
         return {
             user: null
         }
+    },
+    mounted() {
+        fetch(data.url + "api/user", { credentials: "include"}).then((res) => {
+            
+            if(res.status >= 200 && res.status <= 299) {
+                console.log(res.status)
+                this.$router.push("/")
+            }
+        })
     },
     methods: {
         async test() {
@@ -89,59 +99,60 @@ export default {
             if(res.status == 422) {
             }
             res.json().then((val) => {
-                console.log(val)
+                let errL = {
+                    user: document.getElementById("userINVALID") as HTMLSpanElement,
+                    pass: document.getElementById("passINVALID") as HTMLSpanElement,
+                    reg: document.getElementById("regINVALID") as HTMLSpanElement,
+                }
+                let err = {
+                    "1": errL.user,
+                    "2": errL.pass,
+                    "3": errL.reg,
+                    "4": errL.reg,
+                }
+                errL.user.textContent = "TEST"
+                for (const datErr in errL) {
+                    errL[(datErr as strDataErr)].textContent = ""
+                }
+                if(res.status >= 200 && res.status <= 299) {
+                    router.push("/")
+                } else {
+                    let errRList = (val.error as string).split("_")
+                    let errNum = (errRList[errRList.length - 1]).split("")[0]
+                    let ErrText = val.error_message.split(":")[0]
+                    console.log(errNum)
+                    let eleSp = (err as any)[errNum] as HTMLSpanElement
+                    
+                    eleSp.textContent = ErrText
+                    
+                }
             })
-        }).catch((res) => {
+            
+
+
             // res.json().then((val: any) => {
             //     console.log(val)
             // })
         })
 
-        },
-        async Rb() {
-
-            fetch("http://localhost:3200/test/", {
-                method: "POST",
-                credentials: "include",
-                
-            }).then((res) => {
-                res.json().then((js) => {
-                    this.user = js.name
-                })
-            })
-            
-            
-        },
-        async RG() {
-
-            fetch("http://localhost:3200/test/", {
-                method: "GET",
-                credentials: "include",
-    
-            }).then((res) => {
-                res.json().then((js) => {
-                this.user = js.name
-            })
-        })
+        }
 
 
-}
+
+
     }
 }
 </script>
 <template>
     <div class="register">
-        <button @click="RegBTN">test</button>
-        USERNAME: {{ user }}
-        <button @click="Rb">test text POST</button>
-        <button @click="RG">test text GET</button>
+
             <div class="login border-2 p-4 rounded-lg">
-            <div class="text">
+            <div class="text font-bold text-lg ">
                 Register your Conspirey account
             </div>
 
-            <br />
             <span id="userINVALID" class="text-sm text-red-500 font-bold"></span>
+            <br />
             <input id="userR" class="
             form-control
             block
@@ -160,8 +171,10 @@ export default {
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
             " type="text"  placeholder="Enter username..." />
             <span class="text-gray-500 text-sm">username has to be from 3-16 characters long, <br> can contain numbers, latin letters, _ symbol</span>
-            <div class="di my-5"></div>
+            <!-- <div class="di"></div> -->
+            <br />
             <span id="passINVALID" class="text-sm text-red-500 font-bold"></span>
+            <br />
             <input id="passR" class="
             form-control
             block
@@ -180,8 +193,10 @@ export default {
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
             " type="password" placeholder="Enter password..." />
             <span class="text-gray-500 text-sm">password has to be from 8-32 characters</span> <br>
-            <button @click="REGBTN" class="mt-4 border-2 border-white rounded-md p-2 hover:drop-shadow-lg" id="btnReg">Register Account</button>
+            <button @click="RegBTN" class="mt-4 border-2 border-white rounded-md p-2 hover:drop-shadow-lg" id="btnReg">Register Account</button>
+            <br />
             <span id="regINVALID" class="text-sm text-red-500 font-bold"></span>
+            <br />
             <div class="war">
                 <span class="text-gray-500 text-sm">THERE ARE NO WAYS TO CHANGE PASSWORD</span>
             </div>
