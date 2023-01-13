545edadbcbe0601eb5895d382900847d32fa8603
mrredo <mrredogt@gmail.com>
2023-01-13 23:10:04 +0200

    added popup

--- src/components/elements/user.vue
+++ src/components/elements/user.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
 //imports
+import {User} from "../type"
+import data from "../../data"
 import Swal from 'sweetalert2';
 defineProps({
     "bstyle": String,
@@ -9,12 +11,36 @@ defineProps({
 export default {
     name: "User element",
     mounted() {
-        document.getElementById("ucard")?.classList.add(this.bstyle)
+        document.getElementById("ucard")?.classList.add(this.bstyle as string)
+    },
+    data() {
+        return {
+            user: {} as User,
+            SucFD: false,
+        }
     },
     methods: {
-        
-        fetchUserData() {
+        async ShowMenu() {
+            await this.fetchUserData()
+            Swal.fire("User Info",this.user.id != ""? `${this.user.name} ${this.user.id}`: `Failed to fetch`)
+        },
 
+        async fetchUserData() /*success */ {
+            let response = await fetch(data.url + "api/user", {credentials: "include"})
+            let json = await response.json()
+            this.user = json
+            // fetch(data.url + "api/user", {credentials: "include"}).then((res) => {
+            //     if(res.status >= 200 && res.status <= 299) {
+            //         res.json().then((js) => {
+                        
+            //             this.user = js
+            //             this.SucFD = true   
+            //         })
+
+            //     }
+            // }).catch(() => {
+            //     this.SucFD = false
+            // })
         }
     },
 
@@ -24,7 +50,7 @@ export default {
 </script> 
 <template>
     <div class="pop">
-        <button id="ucard" >UserCard</button>
+        <button @click="ShowMenu()" id="ucard">UserCard</button>
     </div>
 </template>
 
