6a1a10e58266f40c1600994e81bff78084831bf3
mrredo <mrredogt@gmail.com>
2023-02-25 20:52:32 +0200

    e

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './styles/style.css'
 import './styles/tail.css'
+import './styles/main.css'
 import App from './App.vue'
 import router from './routes/routes'
 import '@imengyu/vue3-context-menu/lib/vue3-context-menu.css'
